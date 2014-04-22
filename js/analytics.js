6276b8f87a92aafb84a60dbc2e3b4544c8291ccb
Wilfred Godfrey <wilfred@catchdesign.co.nz>
2014-04-22 15:26:28 +1200

    Fix analytics

--- js/analytics.js
+++ js/analytics.js
@@ -13,7 +13,7 @@
     m.parentNode.insertBefore(a, m);
   })(window, document, "script", "//www.google-analytics.com/analytics.js", "ga");
 
-  ga("create", "UA-36127587-3", "wilfredjamesgodfrey.github.io");
+  ga("create", "UA-36127587-3", "auto");
 
   ga("send", "pageview");
 
--- js/coffee/analytics.coffee
+++ js/coffee/analytics.coffee
@@ -12,5 +12,5 @@
   m.parentNode.insertBefore a, m
   return
 ) window, document, "script", "//www.google-analytics.com/analytics.js", "ga"
-ga "create", "UA-36127587-3", "wilfredjamesgodfrey.github.io"
+ga "create", "UA-36127587-3", "auto"
 ga "send", "pageview"
